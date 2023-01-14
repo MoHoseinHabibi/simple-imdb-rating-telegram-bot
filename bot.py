f32e8e9fcf97e6e285e88bc95106cec96a83f97d
mohammad <mohammad.of313@gmail.com>
2023-01-14 22:25:52 +0330

    add get_rate functions

--- bot.py
+++ bot.py
@@ -1,20 +1,54 @@
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder
-from telegram.ext import ContextTypes, CommandHandler
+from telegram.ext import ContextTypes, CommandHandler, MessageHandler, filters
 import config
+import requests
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='hi there')
+    start_message = 'به ربات خوش آمدید\n با این ربات میتونید امتیاز imdb فیلم و سریال ها رو ببینید \n کافیه اسم اون فیلم یا سریال رو برفرستید.'
+    await update.message.reply_text(start_message)
+
+
+def get_rating(title: str) -> float:
+    """get movie/series title
+    return rating"""
+    
+    sess = requests.session()
+
+    sess.headers = {
+	    "X-RapidAPI-Key": config.RapidAPI_Key,
+	    "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com"
+    }
+
+    # get title id
+    querystring = {"q": title}
+    url = "https://online-movie-database.p.rapidapi.com/auto-complete"
+    titleid = sess.get(url, params=querystring).json()['d'][0]['id']
+
+    # get rating
+    querystring = {"tconst": titleid}
+    url = "https://online-movie-database.p.rapidapi.com/title/get-ratings"
+    rating = sess.get(url, params=querystring).json()['rating']
+
+    return float(rating)
+
+
+async def search_in_imdb(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_chat_action('typing')
+    await update.message.reply_text(get_rating(update.message.text), quote=True)
+
 
 def main() -> None:
     application = ApplicationBuilder().token(config.TOKEN).build()
 
     application.add_handler(CommandHandler('start', start_command))
+    application.add_handler(MessageHandler(filters.TEXT, search_in_imdb))
     application.run_polling()
+
 
 if __name__ == '__main__':
     main()
